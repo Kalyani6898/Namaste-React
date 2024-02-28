1718744d4125ddd4f48186a021b216ab324dcc40
Kalyani6898 <kalyanideshmukh6898@gmail.com>
2024-02-28 23:30:39 +0530

    episode 1 code

---
+++ App.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("div", { id: "parent", xyz: "abc" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am h1 Tag from child1"),
+    React.createElement("h2", {}, "I am h2 Tag from child1"),
+  ]), React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 Tag from child2"),
+    React.createElement("h2", {}, "I am h2 Tag from child2"),
+  ])
+]);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
